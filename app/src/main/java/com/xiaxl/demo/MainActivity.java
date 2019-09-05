c3fa12f29e872ef65d6317f95159ab245d368001
xiaxl <bjxiaxueliang@corp.netease.com>
2019-09-05 15:38:54 +0800

    [sdcard]读写权限

--- app/src/main/java/com/xiaxl/demo/MainActivity.java
+++ app/src/main/java/com/xiaxl/demo/MainActivity.java
@@ -2,8 +2,10 @@ package com.xiaxl.demo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +22,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // sdcard 读写权限
+        findViewById(R.id.button01).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // sdcard 读写权限
+                verifyStoragePermissions(MainActivity.this);
+            }
+        });
+
         // load patch
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,9 +39,32 @@ public class MainActivity extends Activity {
             }
         });
         //
-        mTextView = (TextView) findViewById(R.id.textView);
-        mTextView.setText("patch patch patch");
+//        mTextView = (TextView) findViewById(R.id.textView);
+//        mTextView.setText("patch patch patch");
 
+    }
+
+    // #####################################################################
+
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    public static void verifyStoragePermissions(Activity activity) {
+
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
